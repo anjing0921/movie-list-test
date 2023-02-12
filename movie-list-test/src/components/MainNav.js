485b819bed0ecd2dbb8070731d3e29d8b831f4eb
annadu <duyuan0921@gmail.com>
2023-02-12 15:43:05 -0800

    MainNav

---
+++ movie-list-test/src/components/MainNav.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import MovieIcon from '@mui/icons-material/Movie';
+import LoginIcon from '@mui/icons-material/Login';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ width: "100%"}}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Trending" icon={<WhatshotIcon />} />
+        <BottomNavigationAction label="Movies" icon={<MovieIcon />} />
+        <BottomNavigationAction label="Login" icon={<LoginIcon />} />
+        <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+       
+      </BottomNavigation>
+    </Box>
+  );
+}
