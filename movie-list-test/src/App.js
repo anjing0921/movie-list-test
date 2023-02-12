932d0772f7a3f42192cc3e73317603884808e8db
annadu <duyuan0921@gmail.com>
2023-02-12 14:30:17 -0800

    import Header

--- movie-list-test/src/App.js
+++ movie-list-test/src/App.js
@@ -1,13 +1,12 @@
-
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      
-        <span>Header</span>
-      
-    </div>
+    <>
+      <Header/>
+      <div className="App">movie</div>
+    </>
   );
 }
 
