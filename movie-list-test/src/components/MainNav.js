1deefaf34722db1ad0948074c0215ca8f2dfb40c
annadu <duyuan0921@gmail.com>
2023-02-12 20:21:37 -0800

    add the path

--- movie-list-test/src/components/MainNav.js
+++ movie-list-test/src/components/MainNav.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -8,11 +10,25 @@ import LoginIcon from '@mui/icons-material/Login';
 import SearchIcon from '@mui/icons-material/Search';
 
 
+
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (value === 0) {
+      navigate("/");
+    } else if (value === 1) {
+      navigate("/movies");
+    } else if (value === 2) {
+      navigate("/login");
+    } else if (value === 3) {
+      navigate("/search");
+    }
+  }, [value, navigate]);
 
   return (
-    <Box sx={{ width: "100%"}}>
+    <Box sx={{ width: "100%",position: 'fixed', bottom: 0, left: 0, right: 0 ,bgcolor: '#cfe8fc'}}>
       <BottomNavigation
         showLabels
         value={value}
