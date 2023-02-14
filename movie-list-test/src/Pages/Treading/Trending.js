3a1380c69e746e3967d8cf51bffcec65ad4a57d1
annadu <duyuan0921@gmail.com>
2023-02-13 22:10:10 -0800

    import single content

--- movie-list-test/src/Pages/Treading/Trending.js
+++ movie-list-test/src/Pages/Treading/Trending.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 const Trending = () => {
   const [page, setPage] = useState(1);
@@ -11,7 +12,7 @@ const Trending = () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
     );
-    console.log(data.results);
+    //console.log(data.results);
     setContent(data.results);
   };
   useEffect(() => {
@@ -21,6 +22,18 @@ const Trending = () => {
   return (
     <div>
       <span className="pageTitle">Trending</span>
+      <div>
+        {content.map((c)=> (
+          <SingleContent 
+          key={c.id}
+          id={c.id}
+          poster={c.poster_path}
+          title={c.title || c.name}
+          date={c.first_air_date || c.release_date}
+          media_type={c.media_type}
+          vote_average={c.vote_average}/>
+        ))}
+      </div>
     </div>
   );
 };
