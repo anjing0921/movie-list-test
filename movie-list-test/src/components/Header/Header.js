78ac5ed936fb8dba5bc74e937941cd74a1282ee2
annadu <duyuan0921@gmail.com>
2023-02-13 19:20:39 -0800

    header done

--- movie-list-test/src/components/Header/Header.js
+++ movie-list-test/src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Header.css";
 import LoginButton from "../../Pages/Login/LoginButton";
+import LogoutButton from "../../Pages/Login/LogoutButton";
 //import LogoutButton from "../../Pages/Login/LogoutButton";
 
 const Header = () => {
   return (
     <>
-      <span className="header">🎬 Entertainment Hub 🎥</span>
-      <div>
+      <span className="header">⭐ Movie Stars ⭐ </span>
+      <span className="loginButton">
+        <LogoutButton/>
         <LoginButton />
-      </div>
+      </span>
     </>
   );
 };
