b620e6b96ebb1d04bd616798c634bbab0132956e
annadu <duyuan0921@gmail.com>
2023-02-15 14:47:16 -0800

    add list

--- movie-list-test/src/components/MainNav.js
+++ movie-list-test/src/components/MainNav.js
@@ -8,6 +8,7 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import MovieIcon from '@mui/icons-material/Movie';
 import LoginIcon from '@mui/icons-material/Login';
 import SearchIcon from '@mui/icons-material/Search';
+import ListIcon from '@mui/icons-material/List';
 
 
 
@@ -24,6 +25,8 @@ export default function SimpleBottomNavigation() {
       navigate("/login");
     } else if (value === 3) {
       navigate("/search");
+    } else if (value === 4) {
+      navigate("/list");
     }
   }, [value, navigate]);
 
@@ -40,6 +43,7 @@ export default function SimpleBottomNavigation() {
         <BottomNavigationAction label="Movies" icon={<MovieIcon />} />
         <BottomNavigationAction label="Login" icon={<LoginIcon />} />
         <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+        <BottomNavigationAction label="WatchList" icon={<ListIcon />} />
        
       </BottomNavigation>
     </Box>
