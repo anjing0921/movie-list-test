105eb1e7891ccd29f9802ec8b2b53d57f37076ac
annadu <duyuan0921@gmail.com>
2023-02-15 14:47:30 -0800

    add login

---
+++ movie-list-test/src/Pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginButton from './LoginButton'
+
+const Login = () => {
+  return (
+    <div>
+      <LoginButton/>
+      <span className="pageTitle">Login</span>
+      
+    </div>
+  )
+}
+
+export default Login
