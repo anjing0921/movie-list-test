c60b77072017d32777f3e156aa8e2894270c232d
annadu <duyuan0921@gmail.com>
2023-02-14 19:45:01 -0800

    youtube button

---
+++ movie-list-test/src/components/YoutubeButton.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+//import Stack from '@mui/material/Stack';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+//import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+import red from '@mui/material/colors/red';
+
+const theme = createTheme({
+  palette: {
+    primary: red,
+  },
+});
+
+
+
+
+export default function YoutubeButton({video}) {
+  return (
+      <Button variant="contained" 
+      color="error"
+      href={`https://www.youtube.com/watch?v=${video}`}
+      startIcon={<YouTubeIcon />}>
+        Watch the Trailer
+      </Button>
+  );
+}
