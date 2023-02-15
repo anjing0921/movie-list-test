df1ec229ef3338a44f22e6411e56f1f7e9772985
annadu <duyuan0921@gmail.com>
2023-02-14 19:44:53 -0800

    addon button

---
+++ movie-list-test/src/components/AddonButton.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
+export default function AddonButton() {
+  return (
+    <Button variant="contained" href="#contained-buttons">
+        + Add to watch list
+    </Button>
+    
+  );
+}
