b6db30888ec8e104460f251948def6066683c0f9
annadu <duyuan0921@gmail.com>
2023-02-15 14:50:26 -0800

    add function

--- movie-list-test/src/components/AddonButton.js
+++ movie-list-test/src/components/AddonButton.js
@@ -3,8 +3,12 @@ import Button from '@mui/material/Button';
 
 
 export default function AddonButton() {
+
+
   return (
-    <Button variant="contained" href="#contained-buttons">
+    <Button variant="contained" href="#contained-buttons" 
+    //onClick={() => addMovieToWatchlist(movie)}
+    >
         + Add to watch list
     </Button>
     
