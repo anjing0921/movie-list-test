02b776bac254de29f08ab34eb03fd3b05a6a556e
annadu <duyuan0921@gmail.com>
2023-02-16 08:45:11 -0800

    add movie card

---
+++ movie-list-test/src/components/MovieCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import MovieControl from './MovieControl'
+
+
+const MovieCard = ({movie}) => {
+  //console.log(movie.poster_path)
+  return (
+    <>
+    <div>MovieCard</div>
+    <div className="movie-card">
+      <div className="overlay"></div>
+      <img
+        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+        alt={`${movie.title} Poster`}
+      />
+      <MovieControl movie={movie}/>
+    </div>
+    </>
+  )
+}
+
+export default MovieCard
