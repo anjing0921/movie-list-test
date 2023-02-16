2b09edb05a754f3ee9eb39e0b87572dae23cbbe1
annadu <duyuan0921@gmail.com>
2023-02-16 08:44:52 -0800

    add control button

---
+++ movie-list-test/src/components/MovieControl.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const MovieControl = () => {
+
+  // const {
+  //   removeMovieFromWatchlist
+  // } = useContext(GlobalContext);
+
+
+
+  return (
+    <>
+    {/* <div>MovieControl</div> */}
+    <Button variant="outlined" startIcon={<DeleteIcon />}
+            // onClick={() => removeMovieFromWatchlist(movie.id)}
+    >
+        Delete
+      </Button>
+
+    </>
+  )
+}
+
+export default MovieControl
