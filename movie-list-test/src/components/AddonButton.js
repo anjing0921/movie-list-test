aca692e0de582e3b29f3ea1f7b6a17369226a207
annadu <duyuan0921@gmail.com>
2023-02-16 08:45:41 -0800

    add on button

--- movie-list-test/src/components/AddonButton.js
+++ movie-list-test/src/components/AddonButton.js
@@ -2,15 +2,43 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 
 
-export default function AddonButton() {
+export default function AddonButton({content,setWatchList,watchlist}) {
+  //console.log(watchlist)
+  const movie = content
+  console.log(movie)
+  const addMovieToWatchlist = movie =>{
+
+    const newWatchList = [...watchlist];
+    newWatchList.push({
+      id:movie.id,
+      title:movie.title,
+      poster_path:movie.poster_path
+    });
+    setWatchList({
+      newWatchList
+    })
+  }
+  //   console.log(content.id)
+  //   console.log(content.title)
+  //   console.log(content.poster_path)
+  
+  //  console.log(watchlist)
+  // }
+  // console.log(content)
+  // console.log(content.id)
+  // console.log(content.title)
+  // console.log(content.poster_path)
+  
+  // console.log(watchlist)
 
 
   return (
     <Button variant="contained" href="#contained-buttons" 
-    //onClick={() => addMovieToWatchlist(movie)}
+    onClick={addMovieToWatchlist}
     >
         + Add to watch list
     </Button>
     
   );
+
 }
