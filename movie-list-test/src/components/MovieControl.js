baf4e1453ef638994e05495efba55530f88ead9e
annadu <duyuan0921@gmail.com>
2023-02-17 23:05:34 -0800

    hard code test finish

--- movie-list-test/src/components/MovieControl.js
+++ movie-list-test/src/components/MovieControl.js
@@ -4,18 +4,28 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Rating from './rating'
 
 
-const MovieControl = () => {
+const MovieControl = ({movie, setWatchList, watchlist}) => {
 
-  // const {
-  //   removeMovieFromWatchlist
-  // } = useContext(GlobalContext);
+  const removeMovieFromWatchlist = (id) =>{
+    console.log(watchlist)
+    const newWatchList = watchlist.filter((movie) => movie.id !== id );
+    console.log(newWatchList);
+    setWatchList(
+    newWatchList
+     )
+  }
+  // const deleteCard = (id) => {
+  //   //   const newcards = cardsData.filter((card) => card.id !== card_id );
+  //   //   console.log(newcards);
+  //   //   setCardsData(newcards);
+  //   // }; 
 
 
 
   return (
     <>
     <Button variant="outlined" startIcon={<DeleteIcon />}
-            //onClick={() => removeMovieFromWatchlist(movie.id)}
+            onClick={() => removeMovieFromWatchlist(movie.id)}
     >
         Delete
     </Button>
