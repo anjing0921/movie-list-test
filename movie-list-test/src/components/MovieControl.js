e9a586a39c8d6fa693ca7f584beec97a8ae7abac
annadu <duyuan0921@gmail.com>
2023-02-18 00:02:08 -0800

    test function done

--- movie-list-test/src/components/MovieControl.js
+++ movie-list-test/src/components/MovieControl.js
@@ -1,37 +1,61 @@
 import React, { useContext } from "react";
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Rating from './rating'
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Rating from "./rating";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const MovieControl = ({ movie, setWatchList, watchlist }) => {
+  // const removeMovieFromWatchlist = (id) =>{
+  //   console.log(watchlist)
+  //   const newWatchList = watchlist.filter((movie) => movie.id !== id );
+  //   console.log(newWatchList);
+  //   setWatchList(
+  //   newWatchList
+  //    )
+  // }
+  // const deleteWatchList = (id) => {
+  //   const { data } = axios.get('https://movie-star-back-end.herokuapp.com/viewers/2/watchlist');
+  //   console.log(data);
+  //   console.log(movie.id);
+  //   // setWatchList(data)
+  // };
+  // useEffect(() => {
+  //   deleteWatchList();
+  // }, []);
+  // const deleteCard = (id) => {
+  //   //   const newcards = cardsData.filter((card) => card.id !== card_id );
+  //   //   console.log(newcards);
+  //   //   setCardsData(newcards);
 
-const MovieControl = ({movie, setWatchList, watchlist}) => {
+  //   // };
 
-  const removeMovieFromWatchlist = (id) =>{
-    console.log(watchlist)
+  const deleteWatchList = async (id) => {
+    const { data } = await axios.get('https://movie-star-back-end.herokuapp.com/viewers/2/watchlist');
+    console.log(data);
+    console.log(movie.id)
     const newWatchList = watchlist.filter((movie) => movie.id !== id );
     console.log(newWatchList);
     setWatchList(
     newWatchList
      )
-  }
-  // const deleteCard = (id) => {
-  //   //   const newcards = cardsData.filter((card) => card.id !== card_id );
-  //   //   console.log(newcards);
-  //   //   setCardsData(newcards);
-  //   // }; 
-
-
+  };
+  // useEffect(() => {
+  //   deleteWatchList();
+  // }, []);
 
   return (
     <>
-    <Button variant="outlined" startIcon={<DeleteIcon />}
-            onClick={() => removeMovieFromWatchlist(movie.id)}
-    >
+      <Button
+        variant="outlined"
+        startIcon={<DeleteIcon />}
+        onClick={()=>deleteWatchList(movie.id)}
+      >
         Delete
-    </Button>
-    <Rating/>
+      </Button>
+      <Rating />
     </>
-  )
-}
+  );
+};
 
-export default MovieControl
+export default MovieControl;
